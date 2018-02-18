7337d75e2dce348b226a6e26e4257c1b717f93e7
Alec Sands <sandsalecv@gmail.com>
2018-02-17 20:10:59 -0600

    three hexes with drawing starting at the center

--- project/scripts.js
+++ project/scripts.js
@@ -1,9 +1,40 @@
 var svg = d3.select("svg")
 
 var group = svg.append("g")
-  .attr("transform", "translate(250, 250)");
+  .attr("transform", "translate(250, 250)")
+  .attr("class", "first");
+
+var group2 = svg.append("g")
+  .attr("transform", "translate(250, 440)")
+  .attr("class", "first");
+
+var group3 = svg.append("g")
+  .attr("transform", "translate(415, 345)")
+  .attr("class", "first");
 
 var data = [
+  {x: 50, y: 87},
+  {x: -50, y: 87},
+  {x: -100, y: 0},
+  {x: -100, y: 0},
+  {x: -50, y: -87},
+  {x: 50, y: -87},
+  {x: 100, y: 0},
+  // {x: -50, y: -87},
+];
+
+var data2 = [
+  {x: 50, y: -87},
+  {x: 100, y: 0},
+  {x: 50, y: 87},
+  {x: -50, y: 87},
+  {x: -100, y: 0},
+  {x: -100, y: 0},
+  {x: -50, y: -87},
+  // {x: -50, y: -87},
+];
+
+var data3 = [
   {x: -100, y: 0},
   {x: -50, y: -87},
   {x: 50, y: -87},
@@ -41,4 +72,42 @@ path
     .attr("stroke-dashoffset", 0)
   .on("end", function() {
     path.attr("stroke-dasharray", "none");
+  });
+
+var path2 = group2.append("path")
+  .attr("d", line(data2) + "z")
+  .attr("stroke", "steelblue")
+  .attr("stroke-width", "5")
+  .attr("fill", "none");
+
+var totalLength = path.node().getTotalLength();
+
+path2
+  .attr("stroke-dasharray", totalLength + " " + totalLength)
+  .attr("stroke-dashoffset", totalLength)
+  .transition()
+    .duration(4000)
+    // .ease("linear")
+    .attr("stroke-dashoffset", 0)
+  .on("end", function() {
+    path2.attr("stroke-dasharray", "none");
+  });
+
+var path3 = group3.append("path")
+  .attr("d", line(data3) + "z")
+  .attr("stroke", "steelblue")
+  .attr("stroke-width", "5")
+  .attr("fill", "none");
+
+var totalLength = path.node().getTotalLength();
+
+path3
+  .attr("stroke-dasharray", totalLength + " " + totalLength)
+  .attr("stroke-dashoffset", totalLength)
+  .transition()
+    .duration(4000)
+    // .ease("linear")
+    .attr("stroke-dashoffset", 0)
+  .on("end", function() {
+    path3.attr("stroke-dasharray", "none");
   });
